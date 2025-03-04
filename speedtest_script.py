6ac7eb8342812e5ac04c88057d945c2ef3603722
Miles Padot <mpadot@emich.edu>
2025-03-04 13:15:12 -0500

    Speed test that implements the python package called speedtest which allows user to test their network speed

---
+++ speedtest_script.py
@@ -0,0 +1,15 @@
+import speedtest
+
+st = speedtest.Speedtest()
+download_speed = st.download()
+
+
+upload_speed = st.upload()
+
+ping = st.results.ping()
+
+print("Your download speed is: ", download_speed)
+print("Your upload speed is: ", upload_speed)
+print("Your ping is: ", ping)
+
+
